24f49aa37de180764e03ee1d21dc7393d82bc9a6
taroj khan <tarujkhan@yahoo.com>
2020-02-04 03:17:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    
+      @@all
+  end
+
+def print_all
+@@all
+end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
